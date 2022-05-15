c94d00504b083608e4d4fa091702dbff099cf732
gustavoshr <gustavoriquesb2002@gmail.com>
2022-05-15 19:12:04 -0300

    calcquadrado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor referente ao lado do quadrado: ");
+            double lado = double.Parse(Console.ReadLine());
+
+            double area = Math.Pow(lado, 2);
+
+            double perimetro = lado * 4;
+
+            Console.WriteLine($"Área: {area} e Perimetro: {perimetro}.");
+        }
+    }
+}
